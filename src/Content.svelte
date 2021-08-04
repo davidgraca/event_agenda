cfb73f33e2ac05e6bae642e11c66201f05ed4815
David Graça <davidgraca@gmail.com>
2021-08-04 17:17:59 +0100

    first working version

---
+++ src/Content.svelte
@@ -0,0 +1,12 @@
+<script>
+  import { getContext } from "svelte";
+  import Surprise from "./Surprise.svelte";
+  export let info, showModal;
+
+  const { open } = getContext("simple-modal");
+
+  $: {
+    console.log(info);
+    if (showModal == true) open(Surprise, {allData:info});
+  }
+</script>
